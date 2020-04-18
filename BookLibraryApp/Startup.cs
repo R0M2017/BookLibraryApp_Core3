62cbd218b209c286c126ced950beb2cca7f1bc0c
Roman <30502931+R0M2017@users.noreply.github.com>
2020-04-17 22:10:13 -0700

    Added MVC Support

--- BookLibraryApp/Startup.cs
+++ BookLibraryApp/Startup.cs
@@ -16,16 +16,34 @@ namespace BookLibraryApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            } 
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePages();
+                app.UseHsts();
+            }  
+            
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
