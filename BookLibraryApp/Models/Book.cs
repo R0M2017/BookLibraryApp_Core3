95191f138c47b70dcdea7a3dc0db80d3f6aa9f63
Roman <30502931+R0M2017@users.noreply.github.com>
2020-04-17 22:37:29 -0700

    Created EntityFramework Database

---
+++ BookLibraryApp/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLibraryApp.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public double Rating { get; set; }
+        public long ISBN { get; set; }
+        public int NumberPages { get; set; }
+        public string Publisher { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Language { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ BookLibraryApp/Models/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibraryApp.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=library.db");
+    }
+}
