facf50f8727fb7e1c7a480d2f4b333dd995b07c0
Roman <30502931+R0M2017@users.noreply.github.com>
2020-04-18 02:44:11 -0700

    Populated Databse, not sure if fully working, will continue later!

--- BookLibraryApp/Controllers/HomeController.cs
+++ BookLibraryApp/Controllers/HomeController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BookLibraryApp.Models;
 
 namespace BookLibraryApp.Controllers
 {
     public class HomeController : Controller
     {
+        LibraryContext context = new LibraryContext();
         public IActionResult Index()
         {
+            // DbInitializer.Initialize(context);
             return View();
         }
     }
