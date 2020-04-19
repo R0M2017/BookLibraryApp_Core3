516ca12f750a8daf7a0951c4efa1a7dfb5a631d0
Roman <30502931+R0M2017@users.noreply.github.com>
2020-04-19 02:54:59 -0700

    Removed Database and Migrations. I Found a database for books which is huge and used with MySQL, therefore, I'll need to take MySQL route. Might look into connecting MySQL and EntityFramework.

--- BookLibraryApp/Controllers/HomeController.cs
+++ BookLibraryApp/Controllers/HomeController.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using BookLibraryApp.Models;
 
 namespace BookLibraryApp.Controllers
 {
     public class HomeController : Controller
     {
-        LibraryContext context = new LibraryContext();
         public IActionResult Index()
         {
-            // DbInitializer.Initialize(context);
             return View();
         }
         public IActionResult About()
