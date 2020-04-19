2fd8f7fc9e98c61488c7369b1cfefae2d8ec9ae8
Roman <30502931+R0M2017@users.noreply.github.com>
2020-04-19 03:31:45 -0700

    At first I was going to use MySql.Data.EntityFrameworkCore. However, it does not work with EF Core 3.1 yet (Waiting for the 8.0.20). Therefore, I will be using Pomelo.EntityFrameworkCore.MySql which was successful in creating a scaffold.

---
+++ BookLibraryApp/booklibrarydatabase/Bookratings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLibraryApp.booklibrarydatabase
+{
+    public partial class Bookratings
+    {
+        public int UserId { get; set; }
+        public string Isbn { get; set; }
+        public int BookRating { get; set; }
+    }
+}
---
+++ BookLibraryApp/booklibrarydatabase/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLibraryApp.booklibrarydatabase
+{
+    public partial class Books
+    {
+        public string Isbn { get; set; }
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+        public uint? YearOfPublication { get; set; }
+        public string Publisher { get; set; }
+        public string ImageUrlS { get; set; }
+        public string ImageUrlM { get; set; }
+        public string ImageUrlL { get; set; }
+    }
+}
