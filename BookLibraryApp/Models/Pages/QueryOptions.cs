742f8f9c698da1cdef4a909b8330f54de4241d46
Roman <30502931+R0M2017@users.noreply.github.com>
2020-04-20 21:24:12 -0700

    Added a Search and Sort to the Library Pages

--- BookLibraryApp/Models/Pages/QueryOptions.cs
+++ BookLibraryApp/Models/Pages/QueryOptions.cs
@@ -8,6 +8,10 @@ namespace BookLibraryApp.Models.Pages
     public class QueryOptions
     {
         public int CurrentPage { get; set; } = 1;
-        public int PageSize { get; set; } = 16;
+        public int PageSize { get; set; } = 18;
+        public string OrderPropertyName { get; set; }
+        public bool DescendingOrder { get; set; }
+        public string SearchPropertyName { get; set; }
+        public string SearchTerm { get; set; }
     }
 }
