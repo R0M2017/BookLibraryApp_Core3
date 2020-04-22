710ddcf43b43e3fcc855b54c607ddf2a99548f41
Roman <30502931+R0M2017@users.noreply.github.com>
2020-04-22 16:47:17 -0700

    Added database and table creation also

---
+++ MySqlDatabase/MySQLDatabaseCreation.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE BookLibraryDatabase;
+
+CREATE TABLE `Users` (
+  `User-ID` int(11) NOT NULL default '0',
+  `Location` varchar(250) default NULL,
+  `Age` int(11) default NULL,
+  PRIMARY KEY  (`User-ID`)
+) ENGINE=MyISAM;
+
+CREATE TABLE `Books` (
+  `ISBN` varchar(13) binary NOT NULL default '',
+  `Book-Title` varchar(255) default NULL,
+  `Book-Author` varchar(255) default NULL,
+  `Year-Of-Publication` int(10) unsigned default NULL,
+  `Publisher` varchar(255) default NULL,
+  `Image-URL-S` varchar(255) binary default NULL,
+  `Image-URL-M` varchar(255) binary default NULL,
+  `Image-URL-L` varchar(255) binary default NULL,
+  PRIMARY KEY  (`ISBN`)
+) ENGINE=MyISAM;
+
+CREATE TABLE `BookRatings` (
+  `User-ID` int(11) NOT NULL default '0',
+  `ISBN` varchar(13) NOT NULL default '',
+  `Book-Rating` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`User-ID`,`ISBN`)
+) ENGINE=MyISAM;
