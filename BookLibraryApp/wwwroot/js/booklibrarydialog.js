bd034acd1c30d8c9319153c259b8c9f0fab48b39
Roman <30502931+R0M2017@users.noreply.github.com>
2020-05-13 01:34:10 -0700

    added a Camera Controller with the ability to open a dialog with camera and take picture. Next step would be integrating it into the book library and using the image to scann the isbn.

---
+++ BookLibraryApp/wwwroot/js/booklibrarydialog.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    // this initializes the dialog (and uses some common options that I do)
+    $("#dialog").dialog({
+        autoOpen: false, modal: true, show: "blind", hide: "blind"
+    });
+
+    // next add the onclick handler
+    $("#scanbook").click(function () {
+        $("#dialog").dialog("open");
+        return false;
+    });
+});
