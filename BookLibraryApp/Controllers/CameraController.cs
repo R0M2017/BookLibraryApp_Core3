ec3d8199494fbac1319034b81b92b45e4fb627c3
Roman <30502931+R0M2017@users.noreply.github.com>
2020-05-13 12:47:06 -0700

    Webcam works for desktop computers with external webcams. Also, website is able to take pictures and store them in a folder. Lastly, I added the Spire.Barcode. The next step would be implementing the library to read isbn.

--- BookLibraryApp/Controllers/CameraController.cs
+++ BookLibraryApp/Controllers/CameraController.cs
@@ -3,15 +3,68 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace BookLibraryApp.Controllers
 {
     public class CameraController : Controller
     {
+        private readonly IWebHostEnvironment _environment;
+
+        public CameraController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         public IActionResult Capture()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Capture(string name)
+        {
+            var files = HttpContext.Request.Form.Files;
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        var fileName = file.FileName;
+                        var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+                        var fileExtension = Path.GetExtension(fileName);
+                        var newFileName = string.Concat(myUniqueFileName, fileExtension);
+                        var filepath = Path.Combine(_environment.WebRootPath, "webcamimg") + $@"\{newFileName}";
+
+                        if (!string.IsNullOrEmpty(filepath))
+                            StoreInFolder(file, filepath);
+
+                        /*
+                        var imageBytes = System.IO.File.ReadAllBytes(filepath);
+
+                        if (imageBytes != null)
+                            StoreInDatabase(imageBytes);
+                            */
+                    }
+                }
+                return Json(true);
+            }
+            else
+                return Json(false);
+        }
+
+        private void StoreInFolder(IFormFile file, string fileName)
+        {
+            using (FileStream fs = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+        }
+
     }
 }
--- BookLibraryApp/wwwroot/js/booklibrarydialog.js
+++ BookLibraryApp/wwwroot/js/booklibrarydialog.js
@@ -1,7 +1,16 @@
 ﻿$(function () {
     // this initializes the dialog (and uses some common options that I do)
     $("#dialog").dialog({
-        autoOpen: false, modal: true, show: "blind", hide: "blind"
+        autoOpen: false,
+        maxWidth: 700,
+        maxHeight: 500,
+        width: 700,
+        height: 500,
+        modal: true,
+        show: "blind",
+        hide: "blind",
+        closeOnEscape: false,
+        dialogClass: "no-close"
     });
 
     // next add the onclick handler
