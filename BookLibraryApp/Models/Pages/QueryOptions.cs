06ddec1d12cd9816a896cf4182f0e300d3c3afb8
Roman <30502931+R0M2017@users.noreply.github.com>
2020-05-12 17:43:40 -0700

    Fixed the numbering on the page.

--- BookLibraryApp/Models/Pages/QueryOptions.cs
+++ BookLibraryApp/Models/Pages/QueryOptions.cs
@@ -8,7 +8,7 @@ namespace BookLibraryApp.Models.Pages
     public class QueryOptions
     {
         public int CurrentPage { get; set; } = 1;
-        public int PageSize { get; set; } = 36;
+        public int PageSize { get; set; } = 34;
         public string OrderPropertyName { get; set; }
         public bool DescendingOrder { get; set; }
         public string SearchPropertyName { get; set; }
