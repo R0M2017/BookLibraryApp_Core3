f2de975284dabd7608b78d7d1d2f0788dcd0cb49
Roman <30502931+R0M2017@users.noreply.github.com>
2020-05-13 00:55:32 -0700

    Working on adding webcam support

---
+++ BookLibraryApp/Controllers/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibraryApp.Controllers
+{
+    public class CameraController : Controller
+    {
+        public IActionResult Capture()
+        {
+            return View();
+        }
+    }
+}
