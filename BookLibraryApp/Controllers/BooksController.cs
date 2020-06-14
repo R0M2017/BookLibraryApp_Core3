1fcf1231932919ab54fe3d798c41b88641526324
Roman <30502931+R0M2017@users.noreply.github.com>
2020-06-13 22:58:53 -0700

    Removed isbn JSON because I won't be storing any isbn there since I figured out how to get them directly.

--- BookLibraryApp/Controllers/BooksController.cs
+++ BookLibraryApp/Controllers/BooksController.cs
@@ -13,65 +13,39 @@ namespace BookLibraryApp.Controllers
 {
     public class BooksController : Controller
     {
-
         private IRepository repository;
         public BooksController(IRepository repo) => repository = repo;
-
-        public IActionResult Index(QueryOptions options)
-        {
-            return View(repository.GetBooks(options));
-        }
-
+        public IActionResult Index(QueryOptions options) => View(repository.GetBooks(options));
         [HttpPost]
         public IActionResult AddBook(Books book)
         {
             repository.AddBook(book);
             return RedirectToAction(nameof(Index));
         }
-
-        public IActionResult UpdateBook(int bookid)
-        {
-            return View(repository.GetBook(bookid));
-        }
-
+        public IActionResult UpdateBook(int bookid) => View(repository.GetBook(bookid));
         [HttpPost]
         public IActionResult UpdateBook(Books book)
         {
             repository.UpdateBook(book);
             return RedirectToAction(nameof(Index));
         }
-
         public IActionResult UpdateAll()
         {
             ViewBag.UpdateAll = true;
             return View(nameof(Index), repository.Books);
         }
-
         [HttpPost]
         public IActionResult UpdateAll(Books[] books)
         {
             repository.UpdateAll(books);
             return RedirectToAction(nameof(Index));
         }
-
         [HttpPost]
         public IActionResult Delete(Books book)
         {
             repository.Delete(book);
             return RedirectToAction(nameof(Index));
         }
-
-        public isbnStorage[] isbnStorage;
-
-        /*public JsonResult InsertISBN(string isbn)
-        {
-            var js = new JavaScriptSerializer();
-            isbnStorage = js.Deserialize<isbnStorage[]>(isbn);
-            System.Diagnostics.Debug.WriteLine("\n\n Controller recieving information \n\n");
-            //foreach (var i in isbnStorage)
-                //System.Diagnostics.Debug.WriteLine("\n\n" + i + "\n\n");
-                // System.Diagnostics.Debug.WriteLine("\n\n" + isbn + "\n\n");
-                return Json("result");
-        }*/
+        
     }
 }
