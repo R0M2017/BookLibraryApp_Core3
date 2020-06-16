dec91de9e99718672b4e8fb38fdbc7e5177785b0
Roman <30502931+R0M2017@users.noreply.github.com>
2020-06-15 20:11:03 -0700

    At this point, I think the app is done. I'm trying to publish it, but the hosting website doesn't do it right.

--- BookLibraryApp/wwwroot/js/colorChanger.js
+++ BookLibraryApp/wwwroot/js/colorChanger.js
@@ -5,10 +5,28 @@
         var random_color = colors[Math.floor(Math.random() * colors.length)].hex;
         cardimgs[i].style.backgroundColor = random_color;
     }
-    var books = document.getElementsByClassName('book');
-    for (var i = 0; i < books.length; i++) {
+
+
+    var random_book1_color = colors[Math.floor(Math.random() * colors.length)].hex;
+    //var book1 = document.getElementsByClassName('book-1');
+    var book1 = document.getElementById('book-1');
+    book1.style.backgroundColor = random_book1_color;
+
+    var random_book2_color = colors[Math.floor(Math.random() * colors.length)].hex;
+    //var book2 = document.getElementsByClassName('book-2');
+    var book2 = document.getElementById('book-2');
+    book2.style.backgroundColor = random_book2_color;
+
+    var random_book3_color = colors[Math.floor(Math.random() * colors.length)].hex;
+    //var book3 = document.getElementsByClassName('book-3');
+    var book3 = document.getElementById('book-3');
+    book3.style.backgroundColor = random_book3_color;
+
+    /*for (var i = 0; i < book1.length; i++) {
         //var colors = ['#ff0000', '#00ff00', '#0000ff'];
-        var random_color = colors[Math.floor(Math.random() * colors.length)].hex;
-        books[i].style.backgroundColor = random_color;
-    }
+        
+        *//*book1[i].style.backgroundColor = random_color;
+        book2[i].style.backgroundColor = random_color;
+        book3[i].style.backgroundColor = random_color;*//*
+    }*/
 });
