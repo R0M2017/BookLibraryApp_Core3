0c1211abdedf4b2d7749af0ef7451cb99c67dae1
Roman <30502931+R0M2017@users.noreply.github.com>
2020-06-16 18:44:57 -0700

    Worked on design

--- BookLibraryApp/wwwroot/js/colorChanger.js
+++ BookLibraryApp/wwwroot/js/colorChanger.js
@@ -6,18 +6,24 @@
         cardimgs[i].style.backgroundColor = random_color;
     }
 
-
     var random_book1_color = colors[Math.floor(Math.random() * colors.length)].hex;
     var random_book2_color = colors[Math.floor(Math.random() * colors.length)].hex;
     var random_book3_color = colors[Math.floor(Math.random() * colors.length)].hex;
     var book1 = document.getElementById('book-1');
+    // var book1 = document.getElementsByClassName('book1');
     var book2 = document.getElementById('book-2');
+    // var book2 = document.getElementsByClassName('book2');
     var book3 = document.getElementById('book-3');
+    // var book3 = document.getElementsByClassName('book3');
 
-    if (book1 != null && book2 != null && book3 != null) {
+    var random_bookborder_color = colors[Math.floor(Math.random() * colors.length)].hex;
+    var bookborder = document.getElementById('book-border');
+
+    if (book1 != null && book2 != null && book3 != null && bookborder != null) {
         book1.style.backgroundColor = random_book1_color;
         book2.style.backgroundColor = random_book2_color;
         book3.style.backgroundColor = random_book3_color;
+        bookborder.style.borderColor = random_bookborder_color;
     }
 
     /*for (var i = 0; i < book1.length; i++) {
