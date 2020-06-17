63525a2cdaf0dee13b991619031c6ae52bc08da5
Roman <30502931+R0M2017@users.noreply.github.com>
2020-06-16 20:41:56 -0700

    Added better registration password requirements.

--- BookLibraryApp/Models/Accounts.cs
+++ BookLibraryApp/Models/Accounts.cs
@@ -35,7 +35,8 @@ namespace BookLibraryApp.Models
         public DateTime Dateofbirth { get; set; }
         public DateTime Datecreated { get; set; }
         [Required]
-        //[StringLength(8, ErrorMessage = "Password must be 8 characters long")]
+        [StringLength(255, ErrorMessage = "Password cannot be less than 8 characters", MinimumLength = 8)]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Passwords must be at least 8 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         public string Password { get; set; }
         [NotMapped]
         [Required]
